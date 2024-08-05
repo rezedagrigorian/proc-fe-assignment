04c25643ee4b8709de235de09c114b514432fe56
Rada <59962645+rezedagrigorian@users.noreply.github.com>
2024-08-05 09:40:24 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+@import "https://cdn.syncfusion.com/ej2/material.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 100px;
+}
+</style>
