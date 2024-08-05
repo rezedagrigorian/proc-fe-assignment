42773389833db1035c8ffac69d394bb572194270
Rada <59962645+rezedagrigorian@users.noreply.github.com>
2024-08-05 15:56:42 +0000

    Added breadcrumps component

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-row items-center flex-wrap mb-6">
+    <div class="breadcrumb-item">
+      <i class="icons8-home text-slate-500 text-2xl" />
+    </div>
+    <div
+      v-for="(crumb, index) in items"
+      :key="index"
+      class="breadcrumb-item text-slate-500"
+    >
+      <a v-if="index !== items.length - 1" :href="crumb.link">{{ crumb.text }}</a>
+      <span v-else>{{ crumb.text }}</span>
+    </div>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: 'Breadcrumbs',
+    props: {
+      items: {
+        type: Array,
+        required: true,
+        validator: (value) => {
+          return value.every(item => 'text' in item && 'link' in item);
+        }
+      }
+    }
+  };
+</script>
+  
+<style scoped>
+  .breadcrumb-item + .breadcrumb-item::before {
+    content: "/";
+    padding: 0 1rem;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,31 @@
 <template>
   <MainMenu />
-  <p>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/training_profile/12">Profile</RouterLink>
-    </nav>
-  </p>
+  <div class="p-6">
+    <Breadcrumbs :items="breadcrumbs" />
+    <p>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/training_profile/12">Profile</RouterLink>
+      </nav>
+    </p>
+  </div>
 </template>
 
 <script>
 import MainMenu from "@/components/MainMenu.vue";
+import Breadcrumbs from "../components/Breadcrumbs.vue";
 
 export default {
   components: {
     MainMenu,
+    Breadcrumbs
   },
+  data() {
+    return {
+      breadcrumbs: [
+        { text: 'Home', link: '/' },
+      ]
+    };
+  }
 };
 </script>
