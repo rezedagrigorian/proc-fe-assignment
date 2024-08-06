94053f58fae59fe1d5a229fe8a7d9e657b575a71
Rada <59962645+rezedagrigorian@users.noreply.github.com>
2024-08-06 11:01:51 +0000

    Grid table header templates, content box wrapper component

---
+++ src/components/ContentBoxWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="rounded-xl bg-white border border-slate-200 p-5 p-t-6">
+    <div v-if="$slots.title">
+      <slot name="text" />
+    </div>
+    <div v-else>
+      <h1 class="text-slate-800 font-bold leading-8">{{ title }}</h1>
+    </div>
+    <p class="text-slate-500 text-xs mt-1">{{ description }}</p>
+    <div class="mt-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentBoxWrapper',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,55 +2,71 @@
   <MainMenu />
   <div class="p-6">
     <Breadcrumbs :items="breadcrumbs" />
-    <div class="lovely-grid">
-      <GridComponent
-        ref='grid'
-        :dataSource='gridData'
-        :allowFiltering='true'
-      >
-        <ColumnsDirective>
-          <ColumnDirective
-            field='id'
-            headerText='ID'
-            textAlign="Right"
-            width="70"
-            :allowFiltering='false'
-          />
-          <ColumnDirective
-            field='title'
-            headerText='Title'
-            :allowFiltering='false'
-          />
-          <ColumnDirective
-            field='achieved'
-            headerText='Achieved'
-            :template="'achievementTemplate'"
-            :filterTemplate="'statusFilterTemplate'"
-          />
-          <ColumnDirective
-            headerText="Action"
-            :template="'actionTemplate'"
-            filterTemplate="</>"
-          />
-        </ColumnsDirective>
+    <ContentBoxWrapper
+      title="My training profiles"
+      description="Overview of required training profiles"
+    >
+      <div class="lovely-grid">
+        <GridComponent
+          ref='grid'
+          :dataSource='gridData'
+          :allowFiltering='true'
+        >
+          <ColumnsDirective>
+            <ColumnDirective
+              field='id'
+              :headerTemplate="'headerTemplate'"
+              textAlign="Right"
+              width="100"
+              :allowFiltering='false'
+            />
+            <ColumnDirective
+              field='title'
+              :headerTemplate="'headerTemplate'"
+              :allowFiltering='false'
+            />
+            <ColumnDirective
+              field='achieved'
+              :headerTemplate="'headerTemplate'"
+              :template="'achievementTemplate'"
+              :filterTemplate="'statusFilterTemplate'"
+            />
+            <ColumnDirective
+              field="action"
+              :headerTemplate="'headerTemplate'"
+              :template="'actionTemplate'"
+              filterTemplate="</>"
+            />
+          </ColumnsDirective>
 
-        <template v-slot:achievementTemplate="{ data }">
-          <span :class="achievementClass(data.achieved)">
-            {{ data.achieved ? 'Achieved' : 'Failed' }}
-          </span>
-        </template>
+          <template v-slot:achievementTemplate="{ data }">
+            <span :class="achievementClass(data.achieved)">
+              {{ data.achieved ? 'Achieved' : 'Failed' }}
+            </span>
+          </template>
 
-        <template v-slot:actionTemplate="{ data }">
-          <RouterLink :to="`/training_profile/${data.id}`">
-            <i class="icons8-envelope text-slate-500 text-2xl mr-3" />
-          </RouterLink>
-        </template>
+          <template v-slot:actionTemplate="{ data }">
+            <RouterLink :to="`/training_profile/${data.id}`">
+              <i class="icons8-eye text-slate-500 text-2xl mr-3" />
+            </RouterLink>
+          </template>
 
-        <template #statusFilterTemplate>
-          <MagicToggle v-model="achievedFilterValue" :options="achievedFilterOptions" />
-        </template>
-      </GridComponent>
-    </div>
+          <template #statusFilterTemplate>
+            <MagicToggle v-model="achievedFilterValue" :options="achievedFilterOptions" />
+          </template>
+
+          <template v-slot:headerTemplate="{ data }">
+            <div
+              class="text-slate-800 font-bold text-sm flex items-center h-full"
+              :class="{ 'header-cell': data.index != 0, 'header-first-cell': data.index == 0 }"
+            >
+              {{ data.field }}
+              <i v-if="data.index == 0" class="icons8-right-arrow rotate-90 text-blue-500 text-xl ml-1" />
+            </div>
+          </template>
+        </GridComponent>
+      </div>
+    </ContentBoxWrapper>
   </div>
 </template>
 
@@ -63,16 +79,17 @@ import {
 } from '@syncfusion/ej2-vue-grids';
 import { DataManager, ODataAdaptor } from "@syncfusion/ej2-data";
 
-// eslint-disable-next-line
 import MainMenu from "@/components/MainMenu.vue";
 import Breadcrumbs from "../components/Breadcrumbs.vue";
 import MagicToggle from '../components/MagicToggle.vue';
+import ContentBoxWrapper from '../components/ContentBoxWrapper.vue';
 
 export default {
   components: {
     MainMenu,
     Breadcrumbs,
     MagicToggle,
+    ContentBoxWrapper,
     GridComponent,
     ColumnsDirective,
     ColumnDirective
@@ -122,17 +139,25 @@ export default {
 </script>
 
 <style>
-.lovely-grid .e-grid {
-  border-radius: 8px;
-  border: 1px solid theme(colors.slate.200);
-  overflow: hidden;
-}
+  .lovely-grid .e-grid {
+    border-radius: 8px;
+    border: 1px solid theme(colors.slate.200);
+    overflow: hidden;
+  }
 
-.lovely-grid .e-gridheader {
-  border-top: 0;
-}
+  .lovely-grid .e-gridheader {
+    border-top: 0;
+  }
 
-.lovely-grid .e-row {
-  height: 70px;
-}
+  .lovely-grid .e-row, .lovely-grid .e-filterbar, .lovely-grid .e-columnheader {
+    height: 75px;
+  }
+
+  .lovely-grid .header-cell {
+    text-transform: capitalize;
+  }
+
+  .lovely-grid .header-first-cell {
+    text-transform: uppercase;
+  }
 </style>
